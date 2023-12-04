3c5877d2e4df1ac274a8eaa76667234f30a92ab1
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-04 12:00:17 +0530

    - dump problem

--- T2_loops.c
+++ T2_loops.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main(void)
 {
     int num, i, rev = 0;
     printf("Enter number: ");
     scanf("%d", &num);
-    while (num > 0)
-    {
-        int rem = num % 10;
-        rev = (rev * 10) + rem;
-        num /= 10;
+    for ( i = 1; i <= 10; i++){
+        printf("%d  *  %d  =  %d\n", num, i, (num * i));
     }
-    printf("\nReverse of number: %d\n\n", rev);
+    
+    // printf("\nReverse of number: %d\n\n", rev);
     return 0;
 }
