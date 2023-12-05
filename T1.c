543cb64540bcf8821879ef22a162fa36d29cb663
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-05 10:37:42 +0530

    - dump bitwise operations

--- T1.c
+++ T1.c
@@ -31,5 +31,15 @@ int main()
     num3 = ++num1 || ++num2;
     printf("\nAfter pre incr: num1 = %d, num2 = %d, num3 = %d\n", num1, num2, num3);
 
+    // ----------------------------------------------------------------------------
+
+    num1 = 10, num2 = 6;
+    num3 = num1 & num2;
+    printf("\nBitwise AND: %d\n", num3);
+    num3 = num1 | num2;
+    printf("\nBitwise OR: %d\n", num3);
+    num3 = num1 ^ num2;
+    printf("\nBitwise XOR: %d\n", num3);
+    
     return 0;
 }
