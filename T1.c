0e2dc6a09a9df284989d06d8baa02755bd8284bd
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-05 17:17:11 +0530

    - testing switch

--- T1.c
+++ T1.c
@@ -24,7 +24,24 @@ int main()
 
     // -----------------------------------------------------------------------
 
-    
+    int choice = 1;
+    switch (choice)
+    {
+        default:
+        printf("Default\n");
+        // break;
+        case 1:
+            printf("\nOne\n");
+            // break;
+        case 2:
+        printf("Two\n");
+            break;
+        case 3:
+            printf("Three\n");
+            break;
+        
+        
+    }
 
     return 0;
 }
