0f4af7c59ce0c2174357fe6c4011d6c6cfe82394
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-05 13:59:52 +0530

    Update T3_date_validatipon.c

--- T3_date_validatipon.c
+++ T3_date_validatipon.c
@@ -17,7 +17,8 @@ int isValidDate(int day, int month, int year) {
     }
 
     // Validate day based on the month
-    if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && (day < 1 || day > 31)) {
+    if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) 
+                && (day < 1 || day > 31)) {
         return 0; // Invalid day
     } else if ((month == 4 || month == 6 || month == 9 || month == 11) && (day < 1 || day > 30)) {
         return 0; // Invalid day
