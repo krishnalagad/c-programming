66fcc9f3fe31e3fa869e21f212c16f17f364c1a1
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-06 11:34:24 +0530

    - dump while condition criteria

--- T4_practice.c
+++ T4_practice.c
@@ -2,11 +2,21 @@
 
 int main(void)
 {
-    extern int func(float);
-    int a;
-    a = func(3.14);
-    printf("%d\n", a);
-    return 0;
+    // extern int func(float);
+    // int a;
+    // a = func(3.14);
+    // printf("%d\n", a);
+    // return 0;
+
+    int i = 1;
+    while (i < 10){       // condition in while loop in must.
+        printf("%d\n", i++);
+        // if (i > 10){
+        //     break;
+        // }
+        
+    }
+    
 }
 int func(float num)
 // float num;
