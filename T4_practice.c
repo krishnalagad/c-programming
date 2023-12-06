cb5e8736823cf8c302a9f3b0b7e536f8b432a3fc
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-06 10:40:38 +0530

    - dump extern function

--- T4_practice.c
+++ T4_practice.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-    extern int a;
-    a = 20;
-    printf("%d\n", sizeof(a));
+    extern int fun(float);
+    int a;
+    a = fun(3.14);
+    printf("%d\n", a);
+}
+int fun(float num)
+// float num;
+{
+    return ((int) num);
 }
 // int a = 20;
