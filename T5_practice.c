ea85cbc01c012a438c0e3197496938984ca0b152
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-07 11:28:14 +0530

    - dump switch, shift operator

--- T5_practice.c
+++ T5_practice.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
 int main(void) {
-    int i;
-    for(i = 0;i = 5 ; i++) {
-        printf("Hello\n");
-    }
+
+    int a = 10;
+    printf("%d", a>>2);
+    // int ch = 'a';
+    // switch (ch)
+    // {
+    //     case 'a':
+    //         printf("\na\n");
+    //         break;
+    //     case 'b':
+    //         printf("\nb\n");
+    //         break;
+    //     case 'c':
+    //         printf("\nc\n");
+    //         break;
+    //     case 'k' + 'a':
+    //         printf("\nk\n");
+    //         break;
+        
+    //     default:
+    //         printf("Default");
+    //         break;
+    // }
 }
