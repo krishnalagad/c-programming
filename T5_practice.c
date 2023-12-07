3037ec2ae90cf5fbd0bbaf9c08ea7735d786448f
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-07 16:20:20 +0530

    - dump pointer tricky example.

--- T5_practice.c
+++ T5_practice.c
@@ -5,11 +5,32 @@ void solution(int *p) {
     scanf("%d", p);
 }
 
+void calculate(int *e, int *o, int num) {
+    int eCount = 0;
+    int oCount = 0;
+    int i = 1,eSum = 0, oSum = 0;
+    while (eCount < num || oCount < num){
+        if (i % 2){
+            oSum += i;
+            oCount++;
+        } else {
+            eSum += i;
+            eCount++;
+        }
+        i++;
+    }
+    *e = eSum;
+    *o = oSum;
+}
+
 int main(void) {
 
-    int a;
-    int *ptr = &a;
-    solution(ptr);
-    printf("\nValue: %d\n\n", a);
+    // int a;
+    // int *ptr = &a;
+    // solution(ptr);
+    // printf("\nValue: %d\n\n", a);
+    int eSum, oSum;
+    calculate(&eSum, &oSum, 20);
+    printf("\nSum of even numbers: %d \nSum of odd numbers: %d\n", eSum, oSum);
     return 1;
 }
