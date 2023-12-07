221ad6a89dae9b9479a6da390f32c1dbb7837e4a
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-07 14:00:07 +0530

    - dump pointer basics
     - pre-increment and post-increment

--- T5_pointer.c
+++ T5_pointer.c
@@ -6,12 +6,13 @@ int main(void) {
     int b = a++;
     printf("\na = %d, b = %d, ptr = %p\n", a, b, ptr);
     a = 10;
-    b = ++*ptr;     // 
+    b = ++*ptr;     // pre-increment is same as of variables.
     printf("\na = %d, b = %d, ptr = %p\n", a, b, ptr);
     a = 10;
-    b = *ptr++;
+    b = *ptr++;     // In post-increment it increases the pointer address, not actual value
     printf("\na = %d, b = %d, ptr = %p\n", a, b, ptr);
-    a = 10;
-    b = (*ptr)++;
-    printf("\na = %d, b = %d, ptr = %p\n", a, b, ptr);
+    int no = 10;
+    int *p = &no;
+    int c = (*p)++;       // To make post-increment happen, this syntax is required.
+    printf("\nno = %d, c = %d, p = %p\n", no, c, p);
 }
