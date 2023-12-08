2a85ccfd31c1132dae6bf037123919fcc5d75cd8
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-08 15:29:04 +0530

    - dump marathon problem, full testing remaining.

---
+++ T6_marathon_problem.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+    int amount = 100, curAmt, items = 0;
+    do
+    {
+        printf("You have Rs.%d.\nEnter cost of price, you want to purchase Rs.(20/5): ", amount);
+        scanf("%d", &curAmt);
+        if(curAmt < 5 || curAmt > 20) {
+            printf("\nYou can't purchase items of prices other than Rs.20 & Rs.5\n");
+            continue;;
+        }
+        if (amount < curAmt){
+            printf("\nYou dont have enough amount to purchase, try purchasing items of cost Rs.5\n");
+            continue;
+        }
+        
+        amount -= curAmt;
+        items += 1;
+    } while (amount > 0);
+
+    printf("Total items purchased are: %d", items);
+    return 1;
+}
