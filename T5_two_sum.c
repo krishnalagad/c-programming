851e0f86db65b352e95c82b6d6e350941ec8e377
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-09 09:52:50 +0530

    - no fix required.

--- T5_two_sum.c
+++ T5_two_sum.c
@@ -4,8 +4,8 @@ void solution(int *arr, int len, int target, int *res) {
     for (int i = 0; i < len; i++){
         for (int j = 0; j < len - 1; j++){
             if (arr[i] + arr[j] == target){
-                res[0] = i;
-                res[1] = j;
+                res[0] = j;
+                res[1] = i;
                 break;
             }  
         } 
@@ -13,10 +13,10 @@ void solution(int *arr, int len, int target, int *res) {
 }
 
 int main(void) {
-    int arr[] = {2, 3, 4, 6, 7};
+    int arr[] = {2, 3, 4, 6, 7, 12, 23, 45, 56, 67, 78, 100, 150};
     int res[2];
     int len = sizeof(arr) / sizeof(arr[0]);
-    int target = 9;
+    int target = 250;
     solution(arr, len, target, res);
     printf("\n[%d,%d]\n", res[0], res[1]);
     return 0;
