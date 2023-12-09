b33363143dfd282ffa5e7c03b27f21c43d7eb40c
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-09 16:07:56 +0530

    - dump

--- T7_practice.c
+++ T7_practice.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void display(int (*)[]);
+
+void display(int (*arr)[3]) {
+    // printf("\nAddresses: \n");
+    // printf("%d\n", (void *)arr);
+    // arr++;
+    // printf("%d\n", (void *)arr);
+    // arr++;
+    // printf("%d\n", (void *)arr);
+    printf("\nElements:\n");
+
+    for (int i = 0; i < 2; i++){
+        for (int j = 0; j < 3; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    
+}
+
 int main(void) {
 
     int a = 10, b = 20, c = 30;
@@ -14,11 +34,16 @@ int main(void) {
 
     // pointer to pointer (2D array)
     int row = 3, col = 4;
-    int **ptr = (int**) malloc(r * sizeof(int*));
-    for (int i = 0; i < r; i++){
-        ptr[i] = (int*) malloc(c * sizeof(int));
+    int **ptr = (int**) malloc(row * sizeof(int*));
+    for (int i = 0; i < row; i++){
+        ptr[i] = (int*) malloc(col * sizeof(int));
     }
+    printf("%d\n", ptr);
     
+    // ------------------------------------------------------------------------------------
+
+    int arr[2][3] = {2,3,4,5,6,6};
+    display(arr);
 
     return 1;
 }
