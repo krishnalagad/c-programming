02136a2ac07a7357345e081052de2181be6dd63d
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-09 09:51:11 +0530

    - dump solution of two sum, testing remaining.

--- T5_two_sum.c
+++ T5_two_sum.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
-int solution(int *arr, int target) {    
-    // int len = sizeof(arr) / sizeof(arr[0]);
-    printf("%d\n", sizeof(arr));
-    // printf("%d\n", len);
-    // if (len < 2){
-    //     return 0;
-    // }
-    printf("hiiii");
+void solution(int *arr, int len, int target, int *res) {    
+    for (int i = 0; i < len; i++){
+        for (int j = 0; j < len - 1; j++){
+            if (arr[i] + arr[j] == target){
+                res[0] = i;
+                res[1] = j;
+                break;
+            }  
+        } 
+    } 
 }
 
 int main(void) {
-
     int arr[] = {2, 3, 4, 6, 7};
+    int res[2];
+    int len = sizeof(arr) / sizeof(arr[0]);
     int target = 9;
-    int res = solution(arr, target);
+    solution(arr, len, target, res);
+    printf("\n[%d,%d]\n", res[0], res[1]);
     return 0;
 }
 
