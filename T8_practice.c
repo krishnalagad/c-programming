eefd4bd71314d581157e0ec67f40556afcee7131
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-11 10:27:06 +0530

    - dump typedef

--- T8_practice.c
+++ T8_practice.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-struct Student{
+struct T8_practice
+{
     int marks[5];
-}stud;  
+}stud;
+
 
 int main(void) {
     int arr[5] = {10, 20, 30, 40, 50};
 
-    struct Student s1;
+    typedef struct T8_practice Student;
+    Student s1;
     int len = sizeof(arr) / sizeof(arr[0]);
     for (int i = 0; i < len; i++)
         stud.marks[i] = arr[i];
