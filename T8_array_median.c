154f6062449bf3c6ccf3240711c1b804c35878d5
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-11 15:21:26 +0530

    - dump array median problem solution, fix required

---
+++ T8_array_median.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void sortArray(int *arr, int len) {
+    for (int i = 0; i < len; i++){
+        for (int j = 0; j < len - i - 1; j++){
+            if (arr[j] > arr[j + 1]){
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            } 
+        }
+    } 
+}
+
+int solution(int *arr1, int len1, int *arr2, int len2) {
+    int mergedArr[len1 + len2];
+    int index = 0;
+    for (int i = 0; i < len1; i++){
+        mergedArr[index] = arr1[i];
+        index += 1;
+    }
+    for (int i = 0; i < len2; i++){
+        mergedArr[index] = arr2[i];
+        index += 1;
+    }
+    sortArray(mergedArr, (len1 + len2));
+
+    if ((len1 + len2) % 2){
+        int outIndex = ((len1 + len2) / 2);
+        return mergedArr[outIndex];
+    } else{
+        int outIndex = ((len1 + len2) / 2) + 1;
+        int res = (mergedArr[outIndex] + mergedArr[outIndex - 1]) / 2;
+        return res;
+    }
+    // for (int i = 0; i < len1 + len2; i++){
+    //     printf("%d - > ", mergedArr[i]);
+    // }
+    // printf("NULL\n");
+    return 1;
+}
+int main(void) {
+    int arr1[] = {1,3};
+    int len1 = sizeof(arr1) / sizeof(arr1[0]);
+    int arr2[] = {3};
+    int len2 = sizeof(arr2)/ sizeof(arr2[0]);
+    int result = solution(arr1, len1, arr2, len2);
+    printf("Median of two arrays is: %d\n", result);
+    return 0;
+}
