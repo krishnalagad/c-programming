e5be52a1c472bc35b8b223289a4e86e2eb6b541f
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-12 11:33:03 +0530

    - initial commit of the day

---
+++ T9_practice.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// int a;
+// static int b;
+int main(void) {
+    // int a;
+    // static int b;
+    // printf("%d \n", a);
+    // printf("%d \n", b);
+
+    int a = 66;
+    printf("\n%d %d %d\n", a, ++a, a++);
+    
+    return 0;
+}
