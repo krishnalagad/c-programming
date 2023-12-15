ad464ff8277ff9fca0c15e9c3c6aa9f3ac036fe9
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-15 16:18:13 +0530

    - dump imp notes

--- CPP_Codes/T12_practice.cpp
+++ CPP_Codes/T12_practice.cpp
@@ -4,10 +4,29 @@ using namespace std;
 
 // << insertion opertor
 // >> extraction operator
+// poiters are flexible whereas references are rigid.
+// We can return global variable by reference, but cant local variable.(gives warning)
+// Unlike pointer, References has to be initilize on the same line.
+// Unlike pointer, Array of references is not allowed.
+
+void fun(int &a) {    // reference
+    a += 2;
+}
+
+void fun(int *a) {
+    int x;
+    const int *const ptr = &x;  // constant pointer to constant data (integer)
+    a += 2;
+}
 
 int main(void) {
+    int a = 20;
+    fun(&a);
+    cout<<a<<endl;
+
     int *arr = new int[10];     // array declaration approach
     char *cptr = new char[20];
+    int *scores = new int[10];
 
     char str[] = "Learning is good";
     int len = strlen(str);
@@ -19,5 +38,6 @@ int main(void) {
     cout<<"Addition of two numbers: "<<(num1 + num2)<<endl;
 
     delete arr, cptr;   // use delete to avoid memory leakage.
+    delete []scores;    // delete all the array not only 1st block of it.
     return 0;
 }
