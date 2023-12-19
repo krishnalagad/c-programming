c4f77a618180cd151162bab474fed9e6f9bc7906
Krishna Lagad <krishnalagad2@gmail.com>
2023-12-19 19:46:56 +0530

    - dump solution

--- CPP_Codes/T13_practice.cpp
+++ CPP_Codes/T13_practice.cpp
@@ -6,3 +6,46 @@
     Product p2("Krishna");
     Product p3 = p1 + p2;
 */
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Product {
+    char *name;
+    public:
+        Product(): name(nullptr){}
+        Product(char *name) {
+            this->name = new char[strlen(name) + 1];
+            strcpy(this->name, name);
+        }
+        ~Product() {
+            delete []name;
+        }
+
+        char *getName() const { return name; }
+        void setName(char *name_) { 
+            delete []name;  // delete existing memory if present.
+            this->name = new char[strlen(name_) + 1];
+            strcpy(this->name, name_); 
+        }  
+
+        Product operator+(Product &product) {
+            Product temp;
+            temp.name = new char[strlen(this->name) + strlen(product.name) + 1];
+            strcpy(temp.name, this->name);
+            strcat(temp.name, product.name);
+            return temp;
+        } 
+        friend void operator<<(ostream &os, const Product &product) {
+            os<<"\n"<<product.getName()<<endl;
+        }
+};
+
+int main(void) {
+    Product p1("Hello");
+    Product p2("Krishna");
+    Product p3 = p1 + p2;
+    cout<<p3;
+    return 0;
+}
