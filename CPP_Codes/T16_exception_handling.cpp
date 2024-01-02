1e2db0a28bdcad23aaa3e1dbad93a7e434a875c5
Krishna Lagad <krishnalagad2@gmail.com>
2024-01-02 15:27:41 +0530

    - dump exception handling with custom exceptions

--- CPP_Codes/T16_exception_handling.cpp
+++ CPP_Codes/T16_exception_handling.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 #include <cstring>
 
+class LowBalanceException: public std::exception {
+    std::string message;
+    public:
+        LowBalanceException() {
+            this->message = "Low balance in account!!";
+        }
+        LowBalanceException(std::string message) {
+            this->message = message;
+        }
+        std::string what() {
+            return this->message;
+        }
+};
+
+class Account {
+    private: 
+        int balance;
+    public:
+        Account(): balance(10000) {}
+        void withdraw(int amount) {
+            if(this->balance < 5000)
+                throw LowBalanceException("Balance should not go lower than 5000.\n");
+            if ((this->balance - amount) < 5000)
+                throw LowBalanceException("Balance should not go lower than 5000.\n");
+            this->balance -= amount;
+            std::cout<<"\nRemaining Balance: "<<this->balance;
+        }
+};
+
 class MyException: public std::exception {
     char message[100];
     public:
@@ -42,13 +71,18 @@ void checkCustomException(int num) {
 }
 
 int main() {
-    std::cout<<divide(12, 0);
+    Account a1;
+    // std::cout<<divide(12, 0);
     try {
-        checkCustomException(1);
+        // checkCustomException(1);
+        a1.withdraw(2000);
+        a1.withdraw(4000);
     } catch (const CustomException& ex) {
         std::cout<<"\nCustom Exception caught: "<<ex.what();
     } catch (MyException ex) {
         std::cout<<"\nMyException caught: "<<ex.what();
+    } catch (LowBalanceException ex) {
+        std::cout<<"\nLow balance exception caught: "<<ex.what();
     }
     return 0;
 }
