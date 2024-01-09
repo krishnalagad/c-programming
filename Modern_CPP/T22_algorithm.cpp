27ebef550176f63c7c0558629624ef651a826e08
Krishna Lagad <krishnalagad2@gmail.com>
2024-01-09 10:36:01 +0530

    - dump custom find method implementation

--- Modern_CPP/T22_algorithm.cpp
+++ Modern_CPP/T22_algorithm.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 #include <algorithm>
 
+// custom find method implementation
+template <typename T>
+T* find(T *startPtr, T* lastPtr, T key) {
+    while(startPtr != lastPtr) {
+        if (key == *startPtr)
+            return startPtr;
+        ++startPtr;
+    }
+    return startPtr;
+}
+
 int main() {
+    // find element from array
+    int arr[] = {1,2,3,4,5,6,7,8,9,9,0};
+    int *ptr = std::find(std::begin(arr), std::end(arr), 1);
+    if(ptr == std::end(arr))
+        std::cout<<"Element not found!!"<<std::endl;
+    else {
+        std::cout<<"Base address: "<<arr<<std::endl;
+        std::cout<<"Address where element is present: "<<ptr<<"\nIndex: "<<ptr - arr<<std::endl;
+    }
     return 0;
 }
