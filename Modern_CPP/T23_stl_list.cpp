70caaba8bedadae6e6d489156ebfa39d0ff8a462
Krishna Lagad <krishnalagad2@gmail.com>
2024-01-10 15:52:04 +0530

    - dump mid day commit

--- Modern_CPP/T23_stl_list.cpp
+++ Modern_CPP/T23_stl_list.cpp
@@ -4,17 +4,19 @@
 #include <list>
 #include <vector>
 
-void display(std::list<std::string>& list) {
-    std::cout<<"List elements: ";
-    for(std::string str: list)
+template <typename T>
+void display(T& list) {
+    std::cout<<"Elements: ";
+    for(auto str: list)
         std::cout<<str<<" --> ";
     std::cout<<std::endl;
 }
 
 int main() {
-    std::vector<std::string> vector = {"Krishna", "Lagad", "Bhushan", "Birje"};
-    std::list<std::string> list = {"Krishna", "Lagad", "Bhushan", "Birje"};
+    std::vector<std::string> vector = {"Vector", "Krishna", "Lagad", "Bhushan", "Birje"};
+    std::list<std::string> list = {"List", "Krishna", "Lagad", "Bhushan", "Birje"};
     display(list);
+    display(vector);
 
     // get max element from list.
     std::list<std::string>::iterator it = std::max_element(std::begin(list), std::end(list));
@@ -27,10 +29,14 @@ int main() {
 
     auto it_v = std::begin(vector);     // operations allowed: ptr + int, ptr - int, ptr - ptr, ptr++, ptr--, >, <, >=, <=, ==, !=
     auto it_l = std::begin(list);       // operations allowed: ptr++, ptr--, ==, !=
+    vector.push_back("Temp");
+    list.push_back("Raw");
     ++++it_v;       // pre-increment make changes twice
     (it_l++)++;       // post increment make changes once
     std::cout<<"Vector element: "<<*it_v<<std::endl;
     std::cout<<"List element: "<<*it_l<<std::endl;
+    display(list);
+    display(vector);
 
     return 0;
 }
