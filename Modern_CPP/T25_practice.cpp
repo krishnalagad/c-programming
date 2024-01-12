e73c0f108a56ced2a5683706a6af1e732f7594d8
Krishna Lagad <krishnalagad2@gmail.com>
2024-01-12 15:12:30 +0530

    - dump practice for day 25

--- Modern_CPP/T25_practice.cpp
+++ Modern_CPP/T25_practice.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+
+class Vehicle {
+    private:
+    public: 
+        Vehicle() { std::cout<<"Base()!!"<<std::endl; }
+        virtual void show() { std::cout<<"Base show()!!"<<std::endl; }
+};
+
+class Bike: public Vehicle {
+    private:
+    public:
+        Bike() { std::cout<<"Derived()!!"<<std::endl; }
+        void show() { std::cout<<"Derived show()!!"<<std::endl; }
+};
 
 int main() {
     std::string strArr[] = {"Krishna", "Dilip", "Lagad", "My", "Name"};
@@ -10,5 +25,11 @@ int main() {
     for(int i = 0; i < 5; i++) {
         std::cout<<strArr[i]<<std::endl;
     }
+    std::cout<<std::endl;
+
+    std::vector<Vehicle*> v = {new Vehicle(), new Bike()};
+    for(Vehicle* i: v)
+        i->show();
+
     return 0;
 }
