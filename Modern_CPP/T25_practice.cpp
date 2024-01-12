63df34dc5b1c5c0f8405e01dd17134819678b131
Krishna Lagad <krishnalagad2@gmail.com>
2024-01-12 12:54:04 +0530

    - initial commit of the day

---
+++ Modern_CPP/T25_practice.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    std::string strArr[] = {"Krishna", "Dilip", "Lagad", "My", "Name"};
+    std::sort(std::begin(strArr), std::end(strArr), [](const std::string& s1, const std::string& s2) -> bool {
+        return s1.length() < s2.length();
+    });
+
+    for(int i = 0; i < 5; i++) {
+        std::cout<<strArr[i]<<std::endl;
+    }
+    return 0;
+}
