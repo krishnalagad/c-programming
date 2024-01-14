797712eaea57859f78c9db375f61676f8d822546
Krishna Lagad <krishnalagad2@gmail.com>
2024-01-14 20:42:57 +0530

    - dump vector functionalities

--- Modern_CPP/T25_vector_example.cpp
+++ Modern_CPP/T25_vector_example.cpp
@@ -40,35 +40,106 @@ class Employee {
 };
 
 void addData(std::vector<Employee>& v) {
-    Employee e;
-    std::cin>>e;
-    v.push_back(e);
+    // Employee e;
+    // std::cin>>e;
+    // v.push_back(e);
+
+    // dummy data
+    v = { Employee(1, "Krishna Lagad", 25000), Employee(2, "Jack Sparrow", 23000), 
+        Employee(3, "Don Bradman", 12000)};
+    
 }
 
 void display(const std::vector<Employee>& v) {
+    std::cout<<"\n-----EMPLOYEE DATA-----"<<std::endl;
     for(const Employee& emp : v )
         std::cout<<emp;
 }
 
 void getEmployeeWithMaxSalary(const std::vector<Employee>& v) {
+    std::cout<<"\n-----EMPLOYEE WITH MAX SALARY-----"<<std::endl;
     auto it = std::max_element(std::begin(v), std::end(v), [](const Employee& e1, const Employee& e2){
         return e1.getSalary() < e2.getSalary();
     });
     std::cout<<*it;
 }
 
-void sortVectorBasedOnSalary(std::vector<Employee>& v) {
+void sortAscVectorBasedOnSalary(std::vector<Employee>& v) {
+    std::cout<<"\n-----ASCENDING ORDER ON SALARY-----"<<std::endl;
     std::sort(std::begin(v), std::end(v), [](const Employee& e1, const Employee& e2){
         return e1.getSalary() < e2.getSalary();
     });
     display(v);
 }
 
+void sortDescVectorBasedOnSalary(std::vector<Employee>& v) {
+    std::cout<<"\n-----DESCEDING ORDER ON SALARY-----"<<std::endl;
+    std::sort(std::begin(v), std::end(v), [](const Employee& e1, const Employee& e2){
+        return e1.getSalary() > e2.getSalary();
+    });
+    display(v);
+}
+
+void countTotSalGret20k(std::vector<Employee>& v) {
+    std::cout<<"\n-----EMPLOYEE WITH SALARY > 20K-----"<<std::endl;
+    auto count = std::count_if(std::begin(v), std::end(v), [](const Employee& emp){
+        return emp.getSalary() > 20000;
+    });
+    std::cout<<"Employees with salary greater than 20k: "<<count<<std::endl;
+}
+
+void getEmployeeWithNameUpperBound(std::vector<Employee>& v, const std::string& name) {
+    std::cout << "\n-----SEARCH EMPLOYEE WITH NAME (UPPER_BOUND())-----" << std::endl;
+
+    Employee targetEmployee;
+    targetEmployee.setName(name);
+
+    auto endPtr = std::upper_bound(std::begin(v), std::end(v), targetEmployee, [](const Employee& emp, const Employee& target) {
+        return emp.getName() == target.getName();  // Use less-than to establish a strict weak ordering
+    });
+
+    if (endPtr != std::end(v) && endPtr->getName() == targetEmployee.getName()) {
+        std::string upperName = name;
+        std::transform(upperName.begin(), upperName.end(), upperName.begin(), ::toupper);
+        std::cout << "Employee found with name " << upperName << std::endl;
+        std::cout << *endPtr << std::endl;
+    } else {
+        std::cout << "No record match with name " << name;
+    }
+}
+
+
+
+
+void getEmployeeWithNameFindIf(std::vector<Employee>& v, const std::string name) {
+    std::cout<<"\n-----SEARCH EMPLOYEE WITH NAME (FIND_IF())-----"<<std::endl;
+    Employee e;
+    e.setName(name);
+
+    auto it = std::find_if(std::begin(v), std::end(v), [&name](const Employee& emp){
+        return name == emp.getName();
+    });
+    if (!(it == std::end(v))) {
+        std::string upperName = name;  // non-const copy for modification
+        std::transform(upperName.begin(), upperName.end(), upperName.begin(), ::toupper);
+        std::cout<<"Employee found with name "<<upperName<<std::endl;
+        std::cout<<*it<<std::endl;
+    } else {
+        std::cout<<"No record match with name "<<name;
+    }
+}
+
+
 int main() {
     std::vector<Employee> v;
     addData(v); addData(v); addData(v);
     display(v);
     getEmployeeWithMaxSalary(v);
-    sortVectorBasedOnSalary(v);
+    sortAscVectorBasedOnSalary(v);
+    sortDescVectorBasedOnSalary(v);
+    countTotSalGret20k(v);
+    getEmployeeWithNameFindIf(v, "Krishna Lagad");
+    getEmployeeWithNameUpperBound(v, "Jack Sparrow");   // this uses binary search, so vector shoud be in sorted order
+
     return 0;
 }
