a7c98cdfa72c822c21defcdf677109f5fcda3b3a
Krishna Lagad <krishnalagad2@gmail.com>
2024-01-17 16:41:34 +0530

    - dump stl map with key as an object

--- Modern_CPP/STL/T28_stl_map.cpp
+++ Modern_CPP/STL/T28_stl_map.cpp
@@ -11,7 +11,21 @@ struct CityData {
     CityData() : name(""), population(0), longitude(0.0), latitude(0.0) {}
     CityData(std::string name, uint64_t pop, double lon, double lat)
         : name(std::move(name)), population(pop), longitude(lon), latitude(lat) {}
+
+    bool operator<(const CityData& other) const {
+        return name < other.name; 
+    }
 };
+
+// custom hashing function, ie we pass object as akey to map
+namespace std {
+    template<>
+    struct hash<CityData>{
+       size_t operator()(const CityData& key) {
+        return std::hash<std::string>()(key.name);
+       }
+    }; 
+}
 
 int main() {
     std::map<std::string, CityData> data;
@@ -22,6 +36,12 @@ int main() {
 
     CityData& pune = data["Pune"];
     std::cout<<pune.population<<std::endl;
+
+    std::map<CityData, uint32_t> foundedMap;
+    foundedMap[CityData("Pune", 500000, 29876.3, 92873.9)] = 1178;  // object as a key.
+
+    uint32_t foundedYear = foundedMap[CityData("Pune", 500000, 29876.3, 92873.9)];
+    std::cout<<foundedYear<<std::endl;
 
     return 0;
 }
