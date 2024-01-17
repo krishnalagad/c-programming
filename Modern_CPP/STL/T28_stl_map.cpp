597075a67339793be919d5856fa9bfdc37dab493
Krishna Lagad <krishnalagad2@gmail.com>
2024-01-17 15:54:14 +0530

    - dump map
    - working fine !!

--- Modern_CPP/STL/T28_stl_map.cpp
+++ Modern_CPP/STL/T28_stl_map.cpp
@@ -5,9 +5,23 @@
 struct CityData {
     std::string name;
     uint64_t population;
-    double longitude, latitude;
+    double longitude;
+    double latitude;
+
+    CityData() : name(""), population(0), longitude(0.0), latitude(0.0) {}
+    CityData(std::string name, uint64_t pop, double lon, double lat)
+        : name(std::move(name)), population(pop), longitude(lon), latitude(lat) {}
 };
 
 int main() {
+    std::map<std::string, CityData> data;
+    data["Pune"] = CityData("Pune", 500000, 29876.3, 92873.9);
+    data["Mumbai"] = CityData("Mumbai", 500000, 29876.3, 92873.9);
+    data["Banglore"] = CityData("Banglore", 500000, 29876.3, 92873.9);
+    data["Chennai"] = CityData("Chennai", 500000, 29876.3, 92873.9);
+
+    CityData& pune = data["Pune"];
+    std::cout<<pune.population<<std::endl;
+
     return 0;
 }
