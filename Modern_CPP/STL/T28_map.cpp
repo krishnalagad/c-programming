6f0725a5a092e354c98acb958f276c919aa1be73
Krishna Lagad <krishnalagad2@gmail.com>
2024-01-17 15:18:22 +0530

    - dump map

--- Modern_CPP/STL/T28_map.cpp
+++ Modern_CPP/STL/T28_map.cpp
@@ -1,7 +1,23 @@
-#include <istream>
+#include <iostream>
 #include <map>
 #include <algorithm>
 
 int main() {
+    std::map<int, std::string> map;
+    map[10] = "Krishna";
+    map[20] = "Samiksha";
+    map[30] = "Bhushan";
+    std::map<int, std::string>::iterator it;
+    for(it = map.begin(); it != map.end(); ++it)
+        std::cout<<(*it).first<<" "<<(*it).second<<std::endl;
+    
+    map.insert({40, "Smita"});
+    for(it = map.begin(); it != map.end(); ++it)
+        std::cout<<(*it).first<<" "<<(*it).second<<std::endl;
+
+    map.insert({{50, "Poonam"}, {60, "Ankita"}});
+    for(it = map.begin(); it != map.end(); ++it)
+        std::cout<<(*it).first<<" "<<(*it).second<<std::endl;
+
     return 0;
 }
---
+++ Modern_CPP/STL/T28_stl_map.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+struct CityData {
+    std::string name;
+    uint64_t population;
+    double longitude, latitude;
+};
+
+int main() {
+    return 0;
+}
