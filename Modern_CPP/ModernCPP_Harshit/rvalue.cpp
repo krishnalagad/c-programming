c755ff691f17a7d7b40669b0c8d90b7ad1d0584d
Krishna Lagad <krishnalagad2@gmail.com>
2024-01-23 17:57:54 +0530

    - dump commit of the day
    - transfer ownership of data from function to constructor.

--- Modern_CPP/ModernCPP_Harshit/rvalue.cpp
+++ Modern_CPP/ModernCPP_Harshit/rvalue.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <thread>
 
 int f1(int n1) { return 0; }
 
@@ -9,6 +10,11 @@ int f3(int&& n1) { return 0; }
 
 // takes rvalue ref to std vector of int and returns int by val
 int f4(std::vector<int>&& data) { return 0; }
+
+int demo(std::string&& data) {
+    data = "sjkbdf";
+    return 0;
+}
 
 int main() {
 
@@ -21,5 +27,12 @@ int main() {
     v1.push_back(40);    // any operation after std::move() is illegal, though it works.
     for(int ele: v1) 
         std::cout<<ele<<std::endl;
+
+    std::string str = "Krishna";
+    demo(std::move(str));   // illegal operation below
+    std::this_thread::sleep_for(std::chrono::seconds(20));
+    str = "Lagad";
+    std::cout<<str;
+
     return 0;
 }
