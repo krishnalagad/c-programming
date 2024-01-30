1231aaacdc6b35472b65bf3c2cdb71d066e48b96
Krishna Lagad <krishnalagad2@gmail.com>
2024-01-30 13:18:47 +0530

    - dump mid-day commit of the day

---
+++ Modern_CPP/ModernCPP_Harshit/inheritance/Car.h
@@ -0,0 +1,16 @@
+#ifndef CAR_H
+#define CAR_H
+
+/*
+    A car cannot be copied or moved, default instanciation is not allowed.
+    This class inherits (extends) Vehicle type. As such, it implements all base class abstract function
+*/
+class Car {
+    private:
+        
+    public:
+        Car() {}
+        ~Car() {}
+};
+
+#endif // CAR_H
---
+++ Modern_CPP/ModernCPP_Harshit/inheritance/CarType.h
@@ -0,0 +1,11 @@
+#ifndef CARTYPE_H
+#define CARTYPE_H
+
+enum class CarType {
+    SEDAN,
+    SUV,
+    HATCHBACK,
+    OFF_ROADER
+};
+
+#endif // CARTYPE_H
