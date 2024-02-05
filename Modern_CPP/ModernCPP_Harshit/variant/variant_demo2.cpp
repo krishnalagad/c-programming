c5274c2925681ea911c2fe1f2d9e7adc775d1e9f
Krishna Lagad <krishnalagad2@gmail.com>
2024-02-05 15:55:56 +0530

    - dump variant on stack and heap memory

---
+++ Modern_CPP/ModernCPP_Harshit/variant/variant_demo2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <variant>
+#include <vector>
+
+class Employee {
+    private:
+        
+    public:
+        Employee() = default;
+        void display() { std::cout << "Employee siuuuu!!" << std::endl; }
+        ~Employee() { std::cout << "Emp Object destroyed!!" << std::endl; }
+};
+
+class BusinessOwner {
+    private:
+        
+    public:
+        BusinessOwner() = default;
+        void display() { std::cout << "BusinessOwner siuuuu!!" << std::endl; }
+        ~BusinessOwner() { std::cout << "Businness Object destroyed!!" << std::endl; }
+};
+
+
+
+/*
+    g++ variant_demo2.cpp -o app && time ./app && rm app
+        - std::variant implementation on stack memory
+*/
+int main() {
+    std::vector<std::variant<Employee, BusinessOwner>> data;
+    // data.emplace_back(Employee());   // creates temporary object
+    // data.emplace_back(BusinessOwner());
+
+    data.emplace_back(std::in_place_type<Employee>);    // directly add data at place
+    data.emplace_back(std::in_place_type<BusinessOwner>);
+
+
+    for (std::variant<Employee, BusinessOwner>& v: data) {
+        std::visit([](auto&& val){ val.display(); }, v);
+    }
+    
+    return 0;
+}
