0a0f05eadc0d0795f2699bd831adc4b867486e4d
Krishna Lagad <krishnalagad2@gmail.com>
2024-02-07 12:53:19 +0530

    - dump veriadic function
    - functions taking multiple variable parameters.

---
+++ Modern_CPP/ModernCPP_Harshit/async/fold_expression.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// C++ 17 version 
+template<typename... T>
+auto add(T... n1) {
+    return (n1 + ...);
+}
+
+/*
+    g++ fold_expression.cpp -g -o app && time ./app && rm app
+*/
+int main() {
+    int result = add<int>(10, 20, 30);
+    std::cout << result << std::endl;
+
+    return 0;
+}
