de39c5842c779d10d85f6be3ff936cf9556d1ee2
Krishna Lagad <krishnalagad2@gmail.com>
2024-02-12 14:41:45 +0530

    without thread

--- Modern_CPP/ModernCPP_Harshit/stl_map/composition/Functionalities.cpp
+++ Modern_CPP/ModernCPP_Harshit/stl_map/composition/Functionalities.cpp
@@ -1,9 +1,11 @@
 #include "Functionalities.hpp"
 #include "AccountType.h"
+#include <iostream>
+#include <numeric>
 
 void createObjects(AccountContainer& accounts, MapContainer& data) {
     accounts.emplace_back(std::make_shared<BankAccount>(
-        "Krishna", AccountType::SAVING, 50000.0f, std::make_shared<DebitCard>(
+        "Krishna", AccountType::SAVING, 50001.0f, std::make_shared<DebitCard>(
             109, 1283458736L, "09/34", DebitCardType::RUPAY)));
 
     accounts.emplace_back(std::make_shared<BankAccount>(
@@ -30,4 +32,51 @@ void createObjects(AccountContainer& accounts, MapContainer& data) {
         )
     );
     
+}
+
+float findTotalBankBalance(const MapContainer &data) {
+    if (data.empty()) 
+        throw std::runtime_error("Empt Container!");
+
+    float answer = std::accumulate(data.begin(), data.end(), 
+                    0.0f, [](float total, std::pair<unsigned long, AccountPointer>&& p){
+                        return total + p.second->accountbalance();
+                    });
+    return answer;
+}
+
+unsigned int countAbove50000BalanceAccount(const MapContainer &data) {
+    if (data.empty()) 
+        throw std::runtime_error("Empt Container!");
+    
+    std::size_t count = std::count_if(data.begin(), data.end(),
+                    [](std::pair<unsigned long, AccountPointer>&& p){
+                        return p.second->accountbalance() > 50000;
+                    } );
+    return count;
+}
+
+bool isAnyAccountWithNoCard(const MapContainer &data) {
+    if (data.empty()) 
+        throw std::runtime_error("Empt Container!");
+    
+    bool res = std::any_of(data.begin(), data.end(), 
+                [](std::pair<unsigned long, AccountPointer>&& p){
+                    return p.second->acccountDebitCard() == nullptr;
+                });
+    return res;
+}
+
+std::optional<float> findBalanceById(const MapContainer &data, unsigned long accNumber) {
+    if (data.empty()) 
+        throw std::runtime_error("Empt Container!");
+
+    auto itr = std::find_if(data.begin(), data.end(), 
+                [&](std::pair<unsigned long, AccountPointer>&& p){
+                    return p.second->accountNumber() == accNumber;
+                });
+
+    if (itr == data.end())
+        return std::nullopt;
+    return (*itr).second->accountbalance();
 }
--- Modern_CPP/ModernCPP_Harshit/stl_map/composition/Main.cpp
+++ Modern_CPP/ModernCPP_Harshit/stl_map/composition/Main.cpp
@@ -4,9 +4,33 @@
 /*
     Client code:
         g++ Main.cpp Functionalities.cpp DebitCard.cpp BankAccount.cpp -g -o app && time ./app && rm app
+        g++ Main.cpp Functionalities.cpp DebitCard.cpp BankAccount.cpp -g -o app 2>errors.txt && time ./app && rm app
 */
 int main() {
     MapContainer data;
+    AccountContainer accounts;
+
+    createObjects(accounts, data);
+
+    try {
+        float tot = findTotalBankBalance(data);
+        std::cout << "\nTotal balance: " << tot << std::endl;
+        int count = countAbove50000BalanceAccount(data);
+        std::cout << "Count of above 50000.0f amount: " << count << std::endl;
+
+        if (bool flag = isAnyAccountWithNoCard(data); flag)
+            std::cout << "At least one account exists with no debit card!!" << std::endl;
+        else    
+            std::cout << "No such account exist !!" << std::endl;
+        
+        if (std::optional<float> result = findBalanceById(data, 9000001); result.has_value())
+            std::cout << "Account found with balance: " << result.value() <<std::endl;
+        else    
+            std::cerr << "No such account exist !!" << std::endl;
+    } catch(std::runtime_error& e) {
+        std::cerr << e.what() << '\n';
+    }
+    
     
     return 0;
 }
