281f8ec1319cb8484856f975e52fc4a5eda5f526
Krishna Lagad <krishnalagad2@gmail.com>
2024-03-07 12:50:46 +0530

    add cmake files

---
+++ CMake/course/out/build/CMakeFiles/CMakeCourse.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeCourse"
+  "CMakeCourse.pdb"
+  "CMakeFiles/CMakeCourse.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMakeCourse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
