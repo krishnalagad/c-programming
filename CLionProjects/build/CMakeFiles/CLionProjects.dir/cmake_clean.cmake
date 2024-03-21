b932f738a8f1946b15c1c349f03f33ec80ec61ec
Krishna Lagad <krishnalagad2@gmail.com>
2024-03-21 10:50:33 +0530

    ne project in CLion

---
+++ CLionProjects/build/CMakeFiles/CLionProjects.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CLionProjects"
+  "CLionProjects.pdb"
+  "CMakeFiles/CLionProjects.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLionProjects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
