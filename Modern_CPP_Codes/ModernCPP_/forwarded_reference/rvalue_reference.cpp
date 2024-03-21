3686ca3f9b3558488fb28867f81bcc493e314a87
Krishna Lagad <krishnalagad2@gmail.com>
2024-03-21 09:53:57 +0530

    add rvalue reference example

---
+++ Modern_CPP_Codes/ModernCPP_/forwarded_reference/rvalue_reference.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <functional>
+
+void destination(const std::string& str) {
+    std::cout << "In const lvalue ref fun!! : " << str << std::endl;
+}
+void destination(std::string& str) {
+    std::cout << "In lvalue ref fun!! : " << str << std::endl;
+}
+void destination(std::string&& str) {
+    std::cout << "In rvalue ref fun!! : " << str << std::endl;
+}
+
+/*
+    g++ rvalue_reference.cpp -o app && time ./app && rm app
+*/
+int main() {
+    const std::string str = "Krishna";
+    destination(str);
+    destination(std::move(str));
+    destination(std::ref(str));
+    destination("Krishna");
+
+    return 0;
+}
