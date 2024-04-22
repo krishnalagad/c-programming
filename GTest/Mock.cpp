97f7b2d651109b8cc144ba2ae6e1715cf80c145c
Krishna Lagad <krishnalagad2@gmail.com>
2024-04-22 14:21:07 +0530

    add code for GMock

--- GTest/Mock.cpp
+++ GTest/Mock.cpp
@@ -2,3 +2,37 @@
 #include <vector>
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <functional>
+
+using ::testing::AtLeast;
+using ::testing::Return;
+using ::testing::_;
+
+class DatabaseConnect {
+    public:
+        virtual bool login(std::string username, std::string password) { return true; }
+        virtual bool logout(std::string username) {return true; }
+        virtual int fetchRecord() { return -1; }
+};
+
+using DatabaseRef = std::reference_wrapper<DatabaseConnect>;
+class MyDatabase {
+    DatabaseRef dbC;
+    public:
+        MyDatabase(DatabaseRef _dbC): dbC(_dbC) {}
+        int Init(std::string username, std::string password) {
+            if (dbC.get().login(username, password) !=true) {
+                std::cout << "DB FAILURE!!" << std::endl; return -1;
+            } else {
+                std::cout << "DB SUCCESS!!" << std::endl; return 1;
+            }
+        }
+};
+
+/*
+    g++ Mock.cpp -o test_exec -lgtest -lgtest_main -pthread && time ./test_exec && rm test_exec
+*/
+int main(int argc, char *argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
