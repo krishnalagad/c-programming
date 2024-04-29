d1cdcaafa8f564b1681197621ed0c41d2e496950
krishnalagad <krishnalagad2@gmail.com>
2024-04-29 16:03:58 +0900

    commit code:

--- GTest/Fixtures_Params/TestRunner.cpp
+++ GTest/Fixtures_Params/TestRunner.cpp
@@ -6,20 +6,21 @@
 class ValidatorFixture: public testing::TestWithParam<int> {
     protected:
         Validator validator {5,10};
+        void SetUp() override {}
+        void TearDown() override {}
 };
 
 TEST_P(ValidatorFixture, TestInRange) {
     // Arrange
     int value = GetParam();
-
     // Act
     bool result = validator.inRange(value);
-
     // Assert
     ASSERT_TRUE(result);
 }
 
-INSTANTIATE_TEST_CASE_P(InRangeValues, ValidatorFixture, testing::Values(4,5,6,7,9,10));
+INSTANTIATE_TEST_CASE_P(InRangeValues, ValidatorFixture, 
+                            testing::Values(4,5,6,7,9,10));
 
 /*
     g++ TestRunner.cpp LibraryCode.cpp -o test_exec -lgtest -lgtest_main -lgmock -lgmock_main -pthread &&
