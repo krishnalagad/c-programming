f0946613366ec21b1b8e93601c2ae00a6ebc4bfb
krishnalagad <krishnalagad2@gmail.com>
2024-04-29 15:24:59 +0900

    add files for parameterized test fixtures

---
+++ GTest/Fixtures_Params/LibraryCode.hpp
@@ -0,0 +1,11 @@
+#ifndef LIBRARYCODE_HPP
+#define LIBRARYCODE_HPP
+
+class Validator {
+    private:
+        int mLow, mHigh;
+    public:
+        Validator() = delete;
+};
+
+#endif // LIBRARYCODE_HPP
