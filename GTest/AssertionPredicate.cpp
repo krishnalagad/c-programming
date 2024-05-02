31561d5b65c41f423b1894e6598a4e2e6f4437ea
krishnalagad <krishnalagad2@gmail.com>
2024-05-02 18:11:36 +0900

    commit code

--- GTest/AssertionPredicate.cpp
+++ GTest/AssertionPredicate.cpp
@@ -3,12 +3,16 @@
 
 std::function<bool(int)> evenFunc = [](int num){ return num % 2 == 0; };
 auto numGreatFunc = [](int num1, int num2){ return num1 > num2; };
+std::function<bool(std::string, int len, char ch)> fn = [](std::string str, int len, char ch){ return str.at(0) == ch && str.length() >= len; }; 
 
-TEST(evenFunc, 23) {
+TEST(Predicate, Check1) {
     EXPECT_PRED1(evenFunc, 68);
 }
-TEST(Predicate, Check1) {
+TEST(Predicate, Check2) {
     EXPECT_PRED2(numGreatFunc, 10, 3);
+}
+TEST(Predicate, Check3) {
+    EXPECT_PRED3(fn, "Krishna", 7, 'K');
 }
 
 /*
