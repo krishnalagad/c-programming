2540cbc17356a339536ef0fc6f0a19c6bfe588f8
krishnalagad <krishnalagad2@gmail.com>
2024-05-02 12:59:46 +0900

    initial commit of the day

--- GTest/Mock/Main.cpp
+++ GTest/Mock/Main.cpp
@@ -2,11 +2,11 @@
 #include "UnitTests.hpp"
 
 /*
-    g++ Main.cpp User.cpp UnitTests.hpp MyDatabase.cpp -o test_exec -lgtest -lgtest_main -pthread -lgmock -lgmock_main && 
-    time ./test_exec && 
+    g++ Main.cpp User.cpp UnitTests.hpp MyDatabase.cpp -o test_exec -lgtest -lgtest_main -pthread -lgmock -lgmock_main &&
+    time ./test_exec --gtest_filter=DB_Test.LoginTest:Fixture.UserExistsWithName && 
     rm test_exec
 */
 int main(int argc, char *argv[]) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-}
+} 
