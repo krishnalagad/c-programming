a4f8e6a074c45cc71c770ff381b8648486fb4aa2
krishnalagad <krishnalagad2@gmail.com>
2024-05-06 19:23:59 +0900

    death test case in gtest

---
+++ GTest/DeathTest.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+
+void funcDeathTest() {
+    std::cerr << "Bad thing happened!!";
+    exit(1);
+}
+
+TEST(DeathTest, Check1) {
+    EXPECT_DEATH(funcDeathTest(), "Bad thing happened!!");
+}
+
+/*
+    g++ DeathTest.cpp -o test_exec -lgtest -lgmock -pthread &&
+    time ./test_exec &&
+    rm test_exec
+*/
+int main(int argc, char *argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
