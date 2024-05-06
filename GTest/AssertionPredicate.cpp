4f73c50f9c6af0b5257607e4ee43b4b75d02e3c6
krishnalagad <krishnalagad2@gmail.com>
2024-05-06 17:26:05 +0900

    initial commit of the day

--- GTest/AssertionPredicate.cpp
+++ GTest/AssertionPredicate.cpp
@@ -4,6 +4,7 @@
 std::function<bool(int)> evenFunc = [](int num){ return num % 2 == 0; };
 auto numGreatFunc = [](int num1, int num2){ return num1 > num2; };
 std::function<bool(std::string, int len, char ch)> fn = [](std::string str, int len, char ch){ return str.at(0) == ch && str.length() >= len; }; 
+std::function<bool(int,int,int,int)> fn1 = [](int a, int b, int c, int d){ return (a + b) == (c + d); };
 
 TEST(Predicate, Check1) {
     EXPECT_PRED1(evenFunc, 68);
@@ -14,10 +15,13 @@ TEST(Predicate, Check2) {
 TEST(Predicate, Check3) {
     EXPECT_PRED3(fn, "Krishna", 7, 'K');
 }
+TEST(Predicate, Check4) {
+    EXPECT_PRED4(fn1, 12, 23, 10, 25);
+}
 
 /*
     g++ AssertionPredicate.cpp -o test_exec -pthread -lgtest -lgmock &&
-    time ./test_exec &&
+    time ./test_exec --gtest_filter=Predicate.Check4 &&
     rm test_exec
 */
 int main(int argc, char *argv[]) {
