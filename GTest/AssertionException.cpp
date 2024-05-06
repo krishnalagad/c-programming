d41244e75c6db4c01c212ce04291731e7fbde39b
krishnalagad <krishnalagad2@gmail.com>
2024-05-06 19:13:59 +0900

    exceptions

--- GTest/AssertionException.cpp
+++ GTest/AssertionException.cpp
@@ -10,6 +10,14 @@ TEST(Exception, Check1) {
     ASSERT_THROW(getDivision(10, 0), std::runtime_error);
 }
 
+TEST(Exception, Check2) {
+    ASSERT_ANY_THROW(getDivision(12, 0));
+}
+
+TEST(Exception, Check3) {
+    ASSERT_NO_THROW(getDivision(10, 20));
+}
+
 /*
     g++ AssertionException.cpp -o test_exec -lgtest -lgmock -pthread &&
     time ./test_exec &&
